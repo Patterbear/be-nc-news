402a07c3aef25776d983373b5165f1681a913161
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-08-25 09:57:27 +0100

    respond to task 2 feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,43 +1,31 @@
-const { topicData, articleData, userData, commentData } = require('../db/data/test-data/index');
+const testData = require('../db/data/test-data/index');
 const endpoints = require('../endpoints.json');
 const app = require('../app');
 const seed = require('../db/seeds/seed');
 const db = require('../db/connection');
 const request = require('supertest');
 
-beforeEach(() => {
-    return seed({ topicData, userData, articleData, commentData });
-});
+beforeEach(() => seed(testData));
 
 afterAll(() => {
     return db.end();
 });
 
 describe('GET /api/topics', () => {
-    test('returns array of objects', () => {
+    test('returns array of objects with correct properties', () => {
         return request(app)
         .get('/api/topics')
         .expect(200)
         .then((response) => {
-            const topics = JSON.parse(response.text).topics;
-            
+            const topics = response.body.topics;
+
             expect(Array.isArray(topics)).toBe(true);
 
             for(topic of topics) {
-                expect(typeof topic).toBe('object');
-            }
-        });
-    });
-    test('array objects contain correct properties', () => {
-        return request(app)
-        .get('/api/topics')
-        .expect(200)
-        .then((response) => {
-            const topics = JSON.parse(response.text).topics;
-
-            for(topic of topics) {
-                expect(topic.hasOwnProperty('slug')).toBe(true);
-                expect(topic.hasOwnProperty('description')).toBe(true);
+                expect(topic).toMatchObject({
+                    slug: expect.any(String),
+                    description: expect.any(String)
+                });
             }
         });
     });
--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const endpoints = require('./endpoints.json')
 
 const app = express();
 
-app.use(express.json());
-
-app.use((err, req, res, next) => {
-    console.log(err);
-});
-
 app.get('/api', (req, res) => {
     res.status(200).send(endpoints);
 });
