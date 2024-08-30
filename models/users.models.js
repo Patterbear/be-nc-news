7e9290ae953a933ab2929aafa3f1a2ea25c07f18
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-08-30 11:04:45 +0100

    undo task 16 push

--- models/users.models.js
+++ models/users.models.js
@@ -7,17 +7,3 @@ exports.selectUsers = () => {
         return result.rows;
     });
 }
-
-exports.selectUserByUsername = (username) => {
-    const query = format(`SELECT * FROM users WHERE username = %L`, username);
-
-    return db.query(query)
-    .then((result) => {
-        if(result.rows.length !== 0) {
-            return result.rows[0];
-        } else {
-            return Promise.reject({status: 404, msg: 'not found'});
-        }
-    });
-
-}
