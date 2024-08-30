121dbf1f68678382174150f214aa7ecdf9d07fc6
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-08-30 11:01:14 +0100

    complete task 16

--- models/users.models.js
+++ models/users.models.js
@@ -1,8 +1,23 @@
 const db = require('../db/connection');
+const format = require('pg-format');
 
 exports.selectUsers = () => {
     return db.query('SELECT * FROM users;')
     .then((result) => {
         return result.rows;
     });
+}
+
+exports.selectUserByUsername = (username) => {
+    const query = format(`SELECT * FROM users WHERE username = %L`, username);
+
+    return db.query(query)
+    .then((result) => {
+        if(result.rows.length !== 0) {
+            return result.rows[0];
+        } else {
+            return Promise.reject({status: 404, msg: 'not found'});
+        }
+    });
+
 }
